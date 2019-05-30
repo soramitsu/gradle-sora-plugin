76c36c3afc3aeb655b2254ca328be10ad744f4d6
Bogdan Vaneev <warchantua@gmail.com>
2019-05-30 12:58:37 +0300

    Fix home dir
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- publish.sh
+++ publish.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -ex
-echo gradle.publish.key=${GRADLE_KEY} >> "$HOME_DIR/.gradle/gradle.properties"
-echo gradle.publish.secret=${GRADLE_SECRET} >> "$HOME_DIR/.gradle/gradle.properties"
 
 export PLUGIN_VERSION=${TRAVIS_TAG}
 
