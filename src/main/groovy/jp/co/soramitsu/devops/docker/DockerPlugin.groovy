dd1512aafe61508a126e3fa3ce3e20dc89a6452b
Pavel Varfolomeev <varfolomeev@soramitsu.co.jp>
2022-03-10 13:38:57 +0300

    fix var for tiny

--- src/main/groovy/jp/co/soramitsu/devops/docker/DockerPlugin.groovy
+++ src/main/groovy/jp/co/soramitsu/devops/docker/DockerPlugin.groovy
@@ -209,7 +209,7 @@ class DockerPlugin implements Plugin<Project> {
             // setup tiny https://github.com/krallin/tini
             t.instruction """ENV TINI_VERSION="v0.19.0"
             """ // TODO use variable here
-            t.addFile "https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini", "/tini"
+            t.addFile "https://github.com/krallin/tini/releases/download/\${TINI_VERSION}/tini", "/tini"
             t.runCommand "chmod +x /tini"
             t.entryPoint "/tini", "--"
 
