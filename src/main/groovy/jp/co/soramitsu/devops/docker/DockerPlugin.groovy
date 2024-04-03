1366b4f92b4948238eba8789296c2f4528884435
Marat Biryushev <vfhfnvvv@gmail.com>
2024-04-03 07:06:45 +0300

    change default image

--- src/main/groovy/jp/co/soramitsu/devops/docker/DockerPlugin.groovy
+++ src/main/groovy/jp/co/soramitsu/devops/docker/DockerPlugin.groovy
@@ -296,7 +296,7 @@ class DockerPlugin implements Plugin<Project> {
         } else if (javaVersion == 17) {
             return 'openjdk:17-jdk-slim'
         } else if (javaVersion == 21) {
-            return 'amazoncorretto:21-alpine3.18-jdk'
+            return 'openjdk:21-jdk-slim'
         } else {
             // default fallback version
             return 'openjdk:8-jre-alpine'
